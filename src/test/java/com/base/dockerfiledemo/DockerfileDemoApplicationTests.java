335655f5a11f6e673dbd573433e4b3b33b6fc754
lebron <1227781351@qq.com>
2022-10-15 10:55:33 +0800

    修改

--- src/test/java/com/base/dockerfiledemo/DockerfileDemoApplicationTests.java
+++ src/test/java/com/base/dockerfiledemo/DockerfileDemoApplicationTests.java
@@ -1,14 +1,11 @@
 package com.base.dockerfiledemo;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DockerfileDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
 
-    }
 
 }
