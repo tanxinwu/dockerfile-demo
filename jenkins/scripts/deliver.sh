0735879f4c0127f4295a733a228145ac41d0fc27
lebron <1227781351@qq.com>
2022-10-15 15:24:44 +0800

    修改文件

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,3 +1,5 @@
-echo "hello word!"
-java -jar /var/lib/jenkins/workspace/h3-price/target/dockerfile-demo-0.0.1-SNAPSHOT.jar &
+
+JENKINS_NODE_COOKIE=dontKillMe
+echo "启动项目"
+java -jar /var/lib/jenkins/workspace/h3-price/target/dockerfile-demo-0.0.1-SNAPSHOT.jar >/home/log/h3-price.info.log 2>&1 &
 echo "exec java-jar command!"
