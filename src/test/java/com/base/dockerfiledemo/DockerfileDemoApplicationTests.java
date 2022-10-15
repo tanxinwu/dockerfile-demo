208ce886388306d34504e81d9630e0060d271deb
lebron <1227781351@qq.com>
2022-10-15 11:36:25 +0800

    增加测试

--- src/test/java/com/base/dockerfiledemo/DockerfileDemoApplicationTests.java
+++ src/test/java/com/base/dockerfiledemo/DockerfileDemoApplicationTests.java
@@ -1,11 +1,21 @@
 package com.base.dockerfiledemo;
 
 
+import com.base.dockerfiledemo.model.Step;
+import com.base.dockerfiledemo.service.StepService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DockerfileDemoApplicationTests {
-
-
-
+        @Autowired
+        private StepService stepService;
+        @Test
+        public String test(){
+            Step step  = new Step();
+            step.setCode("123");
+            stepService.insert(step);
+            return "success";
+        }
 }
