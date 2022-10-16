ead9bba811bb769e2026e7bcbe40e4aa3eadefd0
lebron <1227781351@qq.com>
2022-10-16 10:33:46 +0800

    修改文件路径

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,5 @@
 
 JENKINS_NODE_COOKIE=dontKillMe
 echo "启动项目"
-java -jar /var/lib/jenkins/workspace/h3-price/target/dockerfile-demo-0.0.1-SNAPSHOT.jar >/home/log/h3-price.info.log 2>&1 &
+java -jar /var/lib/jenkins/workspace/h3-price/target/dockerfile-demo-0.0.1-SNAPSHOT.jar >/home/logs/h3-price.info.log 2>&1 &
 echo "exec java-jar command!"
