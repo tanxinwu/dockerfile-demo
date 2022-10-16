8d051bd98aa245a7c66dec59ba2cdca5754a4717
lebron <1227781351@qq.com>
2022-10-16 11:30:37 +0800

    增加接口

--- src/main/java/com/base/dockerfiledemo/controller/StepController.java
+++ src/main/java/com/base/dockerfiledemo/controller/StepController.java
@@ -3,7 +3,9 @@ package com.base.dockerfiledemo.controller;
 import com.alibaba.fastjson.JSON;
 import com.base.dockerfiledemo.model.Step;
 import com.base.dockerfiledemo.service.StepService;
+import com.base.dockerfiledemo.vo.SearchRequestVO;
 import com.base.dockerfiledemo.vo.UpdateRequestVO;
+import com.base.dockerfiledemo.vo.response.BaseResponseVO;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -28,6 +30,19 @@ public class StepController {
         Step update = new Step();
         BeanUtils.copyProperties(updateRequestVO,update);
         return  stepService.updateByPrimaryKey(update);
+    }
+
+    @RequestMapping(value = "get",method = RequestMethod.GET)
+    public BaseResponseVO<Step> get(@RequestBody SearchRequestVO searchRequestVO){
+        log.info("查询参数{}",JSON.toJSONString(searchRequestVO));
+        Step step = stepService.selectByPrimaryKey(searchRequestVO.getId());
+        BaseResponseVO<Step> responseVO = new BaseResponseVO<>(0, "查询成功", step);
+        return responseVO;
+    }
+    @RequestMapping(value = "delete",method = RequestMethod.DELETE)
+    public int delete(@RequestBody SearchRequestVO searchRequestVO){
+        log.info("删除参数{}",JSON.toJSONString(searchRequestVO));
+        return stepService.deleteByPrimaryKey(searchRequestVO.getId());
     }
 
 }
---
+++ src/main/java/com/base/dockerfiledemo/vo/SearchRequestVO.java
@@ -0,0 +1,8 @@
+package com.base.dockerfiledemo.vo;
+
+import lombok.Data;
+
+@Data
+public class SearchRequestVO {
+    private Long id;
+}
