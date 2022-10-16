3ea9807f121d31e7b0a6e95a17400000e3af4ff2
lebron <1227781351@qq.com>
2022-10-16 11:06:27 +0800

    修改启动文件

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,14 @@
 
 JENKINS_NODE_COOKIE=dontKillMe
-echo "启动项目"
-java -jar /var/lib/jenkins/workspace/h3-price/target/dockerfile-demo-0.0.1-SNAPSHOT.jar >/home/logs/h3-price.info.log 2>&1 &
-echo "exec java-jar command!"
+echo "  =====关闭Java应用======"
+PROCESS=`ps -ef |grep java |grep -v grep|grep xxx.jar|awk '{print $2}'`
+for i in $PROCESS
+do
+  echo "Kill the $1 process [ $i ]"
+  kill -9 $i
+done
+echo "  =====启动Java应用======"
+nohup java -jar /var/lib/jenkins/workspace/h3-price/target/dockerfile-demo-0.0.1-SNAPSHOT.jar >/home/logs/h3-price.info.log 2>&1 &
+echo $! > savePid.txt
+cat savePid.txt
+rm  savePid.txt
