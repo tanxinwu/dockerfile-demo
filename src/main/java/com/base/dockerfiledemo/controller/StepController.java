cd1c7b913a1472e64dae02f113d176ce554dd4d5
lebron <1227781351@qq.com>
2022-10-16 10:49:59 +0800

    修改日志

--- src/main/java/com/base/dockerfiledemo/controller/StepController.java
+++ src/main/java/com/base/dockerfiledemo/controller/StepController.java
@@ -1,8 +1,10 @@
 package com.base.dockerfiledemo.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.base.dockerfiledemo.model.Step;
 import com.base.dockerfiledemo.service.StepService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/public/step/")
 @AllArgsConstructor
+@Slf4j
 public class StepController {
 
     private final  StepService stepService;
 
     @PostMapping("add")
     public int add(@RequestBody Step record){
+        log.info("新增参数：{}", JSON.toJSONString(record));
         return stepService.insert(record);
     }
 }
